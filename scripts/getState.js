b6f41b6ce571c216ebab3027be4cf71e10665460
Dan <DanNolan99@gmail.com>
2022-03-16 00:32:36 -0400

    adds check and state scripts

---
+++ scripts/getState.js
@@ -0,0 +1,21 @@
+const govAlphaAddr = ""; // <-- TODO: add govAlpha address here
+const tokenAddr = ""; // <-- TODO: add token address here
+
+// first proposal starts at 1
+const PROPOSAL_ID = 1;
+
+async function main() {
+  const govAlpha = await ethers.getContractAt("GovernorAlpha", govAlphaAddr);
+
+  console.log("Proposal:", await govAlpha.proposals(PROPOSAL_ID));
+
+  console.log("Proposal State:", await govAlpha.state(PROPOSAL_ID));
+  console.log("See the ProposalState enum for the definition.");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
